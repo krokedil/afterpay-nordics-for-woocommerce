1d923b1cfee2144d5fd6d832733b7a94b29468de
Niklas Högefjord <niklas@krokedil.se>
2017-11-07 18:42:24 +0100

    Only display installment plans if we have any

--- includes/gateways/class-wc-gateway-afterpay-part-payment.php
+++ includes/gateways/class-wc-gateway-afterpay-part-payment.php
@@ -184,53 +184,57 @@ function init_wc_gateway_afterpay_part_payment_class() {
 			*/
 			$installment_plans = $response->availableInstallmentPlans;
 			if ( ! is_wp_error( $response ) ) {
-				//WC()->session->set( 'afterpay_available_installment_plans', $response->availableInstallmentPlans );
-			   
-			   
-				echo '<p>' . __( 'Please select a payment plan:', 'woocommerce-gateway-afterpay' ) . '</p>';
-				$i = 0;
-				foreach( $installment_plans as $key => $installment_plan ) {
-					
-					if( $installment_plan->installmentProfileNumber < 11 ) {
-						$i ++;
-						$label = sprintf(
-							'%1$s %2$s, %3$s / %4$s',
-							$installment_plan->numberOfInstallments,
-							__( 'months', 'woocommerce-gateway-afterpay' ),
-							wc_price( round( $installment_plan->installmentAmount ) ),
-							__( 'mo', 'woocommerce-gateway-afterpay' )
-						);
-
-
-						// Create payment plan details output
-						if ( $i < 2 ) {
-							$inline_style = 'style="clear:both;position:relative"';
-							$extra_class  = 'visible-ppp';
-						} else {
-							$inline_style = 'style="clear:both;display:none;position:relative"';
-							$extra_class  = '';
-						}
-						
-						$payment_options_details_output .= '<div class="afterpay-ppp-details ' . $extra_class . '" data-campaign="' . $installment_plan->installmentProfileNumber . '" ' . $inline_style . '><small>';
-						
-						$payment_options_details_output .= sprintf( __( 'Start fee: %1$s. Monthly fee: %2$s. Rate: %3$s%5$s. Annual effective rate: %4$s%5$s. Total: %6$s.', 'woocommerce-gateway-afterpay' ),
-						wc_price($installment_plan->startupFee),
-						wc_price($installment_plan->monthlyFee),
-						$installment_plan->interestRate,
-						$installment_plan->effectiveAnnualPercentageRate, 
-						'%', 
-						wc_price($installment_plan->totalAmount) );
-						$payment_options_details_output .= '</small></div>';
-
-						echo '<input type="radio" name="afterpay_installment_plan" id="afterpay-installment-plan-' . $installment_plan->installmentProfileNumber . '" value="' . $installment_plan->installmentProfileNumber . '" ' . checked( $key, 0, false ) . ' />';
-						echo '<label for="afterpay-installment-plan-' . $installment_plan->installmentProfileNumber . '"> ' . $label . '</label>';
-						echo '<br>';
-						
-					}
-				}
 				
-				// Print payment plan details
-				echo $payment_options_details_output;
+				if( $installment_plans ) {
+				
+					//WC()->session->set( 'afterpay_available_installment_plans', $response->availableInstallmentPlans );
+				   
+					echo '<p>' . __( 'Please select a payment plan:', 'woocommerce-gateway-afterpay' ) . '</p>';
+					$i = 0;
+					foreach( $installment_plans as $key => $installment_plan ) {
+						
+						if( $installment_plan->installmentProfileNumber < 11 ) {
+							$i ++;
+							$label = sprintf(
+								'%1$s %2$s, %3$s / %4$s',
+								$installment_plan->numberOfInstallments,
+								__( 'months', 'woocommerce-gateway-afterpay' ),
+								wc_price( round( $installment_plan->installmentAmount ) ),
+								__( 'mo', 'woocommerce-gateway-afterpay' )
+							);
+	
+	
+							// Create payment plan details output
+							if ( $i < 2 ) {
+								$inline_style = 'style="clear:both;position:relative"';
+								$extra_class  = 'visible-ppp';
+							} else {
+								$inline_style = 'style="clear:both;display:none;position:relative"';
+								$extra_class  = '';
+							}
+							
+							$payment_options_details_output .= '<div class="afterpay-ppp-details ' . $extra_class . '" data-campaign="' . $installment_plan->installmentProfileNumber . '" ' . $inline_style . '><small>';
+							
+							$payment_options_details_output .= sprintf( __( 'Start fee: %1$s. Monthly fee: %2$s. Rate: %3$s%5$s. Annual effective rate: %4$s%5$s. Total: %6$s.', 'woocommerce-gateway-afterpay' ),
+							wc_price($installment_plan->startupFee),
+							wc_price($installment_plan->monthlyFee),
+							$installment_plan->interestRate,
+							$installment_plan->effectiveAnnualPercentageRate, 
+							'%', 
+							wc_price($installment_plan->totalAmount) );
+							$payment_options_details_output .= '</small></div>';
+	
+							echo '<input type="radio" name="afterpay_installment_plan" id="afterpay-installment-plan-' . $installment_plan->installmentProfileNumber . '" value="' . $installment_plan->installmentProfileNumber . '" ' . checked( $key, 0, false ) . ' />';
+							echo '<label for="afterpay-installment-plan-' . $installment_plan->installmentProfileNumber . '"> ' . $label . '</label>';
+							echo '<br>';
+							
+						}
+					}
+					
+					// Print payment plan details
+					echo $payment_options_details_output;
+					
+					}
 	
 			} else {
 				//WC()->session->__unset( 'afterpay_installment_plans' );
