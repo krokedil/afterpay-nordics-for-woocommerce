0d5236af818ff5baa6f4c4e3fa6cdbd6b3e14011
Niklas Högefjord <niklas@krokedil.se>
2020-07-13 06:44:23 +0200

    PHPCS

--- includes/requests/helpers/class-wc-afterpay-request-capture-payment.php
+++ includes/requests/helpers/class-wc-afterpay-request-capture-payment.php
@@ -15,11 +15,11 @@ class WC_AfterPay_Request_Capture_Payment extends WC_AfterPay_Request {
 	private $request_method = 'POST';
 
 	public function response( $order_id ) {
-		$wc_order = wc_get_order( $order_id );
+		$wc_order     = wc_get_order( $order_id );
 		$order_number = $wc_order->get_order_number();
-		$request_url = $this->base_url . '/api/v3/orders/' . $order_number . '/captures';
+		$request_url  = $this->base_url . '/api/v3/orders/' . $order_number . '/captures';
 		WC_Gateway_AfterPay_Factory::log( 'Capture payment request sent to: ' . $request_url );
-		$request     = wp_remote_retrieve_body( wp_remote_request( $request_url, $this->get_request_args( $order_id ) ) );
+		$request = wp_remote_retrieve_body( wp_remote_request( $request_url, $this->get_request_args( $order_id ) ) );
 		WC_Gateway_AfterPay_Factory::log( 'Capture payment response: ' . var_export( $request, true ) );
 		return $request;
 	}
@@ -34,19 +34,19 @@ class WC_AfterPay_Request_Capture_Payment extends WC_AfterPay_Request {
 	}
 
 	private function get_request_body( $order_id ) {
-		$order = wc_get_order( $order_id );
+		$order        = wc_get_order( $order_id );
 		$request_body = array(
 			'orderDetails' => array(
 				'totalGrossAmount' => $order->get_total(),
-				'currency' => krokedil_get_order_property( $order_id, 'order_currency' ),
+				'currency'         => krokedil_get_order_property( $order_id, 'order_currency' ),
 			),
 		);
 
 		// Add reference if this is a B2B purchase (first and last name is not sent in authorize request).
 		$customer_category = get_post_meta( $order_id, '_afterpay_customer_category', true );
-		if( 'Company' == $customer_category ) {
+		if ( 'Company' == $customer_category ) {
 			$request_body['references'] = array(
-				'yourReference' => substr( krokedil_get_order_property( $order_id, 'billing_first_name' ) . ' ' . krokedil_get_order_property( $order_id, 'billing_last_name' ), 0, 19),
+				'yourReference' => substr( krokedil_get_order_property( $order_id, 'billing_first_name' ) . ' ' . krokedil_get_order_property( $order_id, 'billing_last_name' ), 0, 19 ),
 			);
 		}
 		return wp_json_encode( $request_body );
